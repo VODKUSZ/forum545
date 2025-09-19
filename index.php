c2ee097530639f83407d64c2aea42a35d36dfb65
KG <KG>
2025-09-19 11:50:18 +0200

    Új téma tömbe rakva és JSON stringé alakítva

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
 $szoveg="";
 if(isset($_POST['topic'])){
-    $szoveg = 'Kaptam új topic post adatot';
+    $topics=[];
+    array_push($topics,$_POST['topic']);
+    $JsonString = json_encode($topics);
+    $szoveg=$JsonString;
 }
 ?>
 <!DOCTYPE html>
