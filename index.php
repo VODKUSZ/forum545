73a80a644fd5d08f2b399c7638105fb15b4d204c
kg <kg>
2025-09-23 10:00:49 +0200

    debug soroktól megtisztitva az index.php

--- index.php
+++ index.php
@@ -11,7 +11,6 @@ $szoveg="";
 if(isset($_POST['topic'])){
     array_push($topics,$_POST['topic']);
     $JsonString = json_encode($topics);
-    $szoveg=$JsonString;
     file_put_contents($fileName,$JsonString);
 }
 ?>
@@ -23,10 +22,9 @@ if(isset($_POST['topic'])){
     <title>Fórum</title>
 </head>
 <body>
-    <?php
-    echo $szoveg;
-    ?>
     <h1>Témák:</h1>
+    <?php
+    ?>
     <form method="post">
         <input type="text" name="topic">
         <input type="submit" value="Add">
